139e2beb5bb3d0d3f1ef501b629ec18b0cea15bc
Marija Iloska <marija.iloska@stonybrook.edu>
2024-09-02 00:41:21 -0400

    included split to train and test data

--- generate_data.m
+++ generate_data.m
@@ -1,6 +1,6 @@
-function [y, H, theta] = generate_data(T, dy, var_h, rt,  p_s, var_y)
+function [y_train, H_train, theta, y_test, H_test] = generate_data(N, N_test, dy, var_h, rt,  p_s, var_y)
 
-
+N_total = N + N_test;
 % Choose random indices to be 0s
 j = datasample(1:dy, p_s, 'replace', false);
 
@@ -12,12 +12,16 @@ theta = normrnd(0, rt, dy, 1);
 theta(j) = 0;
 
 % Create basis functions and data
-%H = sin(normrnd(0, var_h, T, dy));
-H = normrnd(0, var_h, T, dy);
+%H = sin(normrnd(0, var_h, N, dy));
+H = normrnd(0, var_h, N_total, dy);
 
 y = H*theta;
-y = y + mvnrnd(zeros(T,1), var_y*eye(T))';
+y = y + mvnrnd(zeros(N_total,1), var_y*eye(N_total))';
 
+y_train = y(1:N);
+y_test = y(N+1:end);
+H_train = H(1:N,:);
+H_test = H(N+1:end,:);
 
 
 end
