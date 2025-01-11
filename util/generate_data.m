feef46ebdbeaf63ee27f5babf43244a13ecf8116
Marija Iloska <marija.iloska@stonybrook.edu>
2025-01-11 18:31:51 -0500

    generate synthetic data

--- util/generate_data.m
+++ util/generate_data.m
@@ -1,27 +1,38 @@
-function [y_train, H_train, theta, y_test, H_test] = generate_data(N, N_test, dy, var_h, rt,  p_s, var_y)
+function [y_train, X_train, theta, y_test, X_test] = generate_data(N, N_test, P, var_features, var_theta,  num_zeros, var_y)
 
+% Total data to create
 N_total = N + N_test;
+
 % Choose random indices to be 0s
-j = datasample(1:dy, p_s, 'replace', false);
+j = datasample(1:P, num_zeros, 'replace', false);
 
 % Generate random theta in the range between -rt, rt
-theta = normrnd(0, rt, dy, 1);
-%unifrnd(-rt, rt, dy, 1);
+theta = normrnd(0, var_theta, P, 1);
 
 % Set chosen indices to 0s
 theta(j) = 0;
 
 % Create basis functions and data
-%H = sin(normrnd(0, var_h, N, dy));
-H = normrnd(0, var_h, N_total, dy);
+X = zeros(N_total, P);
+%vec = 0.5.^abs((ones(1,P) - (1:P)));
 
-y = H*theta;
+%Sigma_features = toeplitz(vec);
+
+
+Sigma_features = var_features*eye(P);
+for n = 1:N_total
+    X(n,:) = mvnrnd(zeros(1,P), Sigma_features);
+end
+
+% Generate linear model with Gaussian noise
+y = X*theta;
 y = y + mvnrnd(zeros(N_total,1), var_y*eye(N_total))';
 
+% Split into training and test
 y_train = y(1:N);
 y_test = y(N+1:end);
-H_train = H(1:N,:);
-H_test = H(N+1:end,:);
+X_train = X(1:N,:);
+X_test = X(N+1:end,:);
 
 
 end
